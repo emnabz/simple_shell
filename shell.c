6862d72b51c8d826a3d36d3aeecc76a5eff00511
eya-98 <eya.nani@sesame.com.tn>
2020-11-24 16:13:29 +0000

    shell

---
+++ shell.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - the main function of shell
+ * @argc : number of arguments
+ * @argv : the arguments
+ * Return: an int
+ *
+ */
+int main(int argc, char **argv)
+{
+loop();
+return (0);
+}
