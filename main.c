06c23ffec2076f955fb14176a666a191cceb3471
eya-98 <eya.nani@sesame.com.tn>
2020-11-25 20:22:02 +0000

    main function

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+/**
+ * main - main function
+ * @argc : number of arguments
+ * @argv : arguments
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+loop();
+return (0);
+}
